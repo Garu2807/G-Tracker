b6532359efdbeab6e101e13d0424caa21d7971f4
Garu2807 <arutyunyangagik@gmail.com>
2025-06-03 00:47:24 +0300

    NavBar done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import BoardsList from './features/boards/BoardsList';
 import BoardPage from './features/boards/BoardPage';
+import NavBar from './features/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Routes>
-        <Route path="/" element={<BoardsList />} />
+        <Route path="/boards" element={<BoardsList />} />
         <Route path="/boards/:id" element={<BoardPage />} />
       </Routes>
     </div>
---
+++ client/src/features/NavBar/NavBar.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import styles from './NavBar.module.css';
+
+const NavBar = () => {
+  return (
+    <nav className={styles.nav}>
+        <NavLink
+        to="/issues"
+        className={({ isActive }) => (isActive ? styles.active : '')}
+      >
+        Все задачи
+      </NavLink>
+      <NavLink
+        to="/boards"
+        className={({ isActive }) => (isActive ? styles.active : '')}
+      >
+        Доски
+      </NavLink>
+
+    </nav>
+  );
+};
+
+export default NavBar;
